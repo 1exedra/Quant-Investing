31ec8c8aea21047c7c1a20aa89488729dc08e45a
1exedra <exedreye@gmail.com>
2024-07-08 11:22:39 +0530

    Update Listing.py
    Calculating 1yr returns

--- Listing.py
+++ Listing.py
@@ -11,3 +11,7 @@ tickers = sp500['Symbol'].tolist()
 start_date = '2023-01-01'
 end_date = '2024-01-01'
 data = yf.download(tickers, start=start_date, end=end_date)['Adj Close']
+
+data.dropna(axis=1, inplace=True)
+one_year_returns = (data.iloc[-1] - data.iloc[0]) / data.iloc[0]
+top_50_stocks = one_year_returns.nlargest(50)
