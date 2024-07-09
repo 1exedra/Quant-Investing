2c962421eac3af6977a4ed7167721e86909d8f0c
1exedra <exedreye@gmail.com>
2024-07-09 13:40:12 +0530

    Update Listing.py
    Final touchups

--- Listing.py
+++ Listing.py
@@ -4,7 +4,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 pd.options.display.float_format = '{:.6f}'.format
-
 url = 'https://en.wikipedia.org/wiki/List_of_S%26P_500_companies'
 table = pd.read_html(url)
 sp500 = table[0]
@@ -13,18 +12,16 @@ tickers = sp500['Symbol'].tolist()
 start_date = '2023-01-01'
 end_date = '2024-01-01'
 data = yf.download(tickers, start=start_date, end=end_date)['Adj Close']
-
 data.dropna(axis=1, inplace=True)
+
 one_year_returns = (data.iloc[-1] - data.iloc[0]) / data.iloc[0]
 top_50_stocks = one_year_returns.nlargest(50)
 
 portfolio_size = float(input("Enter the size of your portfolio in USD: "))
 equal_weight = portfolio_size / 50
 prices = data.iloc[-1][top_50_stocks.index]
-num_shares = (equal_weight // prices).astype(int)
+num_shares = equal_weight / prices
 
-prices = data.iloc[-1][top_50_stocks.index]
-num_shares = (equal_weight // prices).astype(int)
 portfolio = pd.DataFrame({
     'Stock': top_50_stocks.index,
     '1-Year Return': top_50_stocks.values,
@@ -32,6 +29,11 @@ portfolio = pd.DataFrame({
     'Shares to Buy': num_shares.values,
     'Investment': num_shares.values * prices.values
 })
+
+print(f"Portfolio Size: {portfolio_size}")
+print(f"Equal Weight: {equal_weight}")
+print(f"Prices:\n{prices}")
+print(f"Number of Shares to Buy:\n{num_shares}")
 print(portfolio)
 
 plt.figure(figsize=(14, 7))
@@ -40,4 +42,3 @@ plt.xlabel('Stock')
 plt.ylabel('1-Year Return')
 plt.grid(True)
 plt.show()
-
